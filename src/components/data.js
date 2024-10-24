85ff4d85f039383921a45355637a836ed69ba627
bobur_qalandar0v <qalandarovbobur274@gmail.com>
2024-10-24 17:38:55 +0500

    articles

--- src/components/data.js
+++ src/components/data.js
@@ -3,41 +3,45 @@ export const articlesData = [
     id: 1,
     title: "December 13, 2020",
     subtitle: "12 Things Web Design Your Boss Wants To Know",
-    image: "/public/card1.png",
+    image:
+      "https://s3-alpha-sig.figma.com/img/0d8e/f3d6/85be188780c49a54623a6e2e3817ce11?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Co8RicWzj3OcBAGNtYAVaoPTSk2uhYxB~k9oQsxZ8Mv4z4Dlltt4g8qjeeyaZp~cHBi43zFbT~IhvIbcaP4SDJQRkxKWaDjC8OlPqiDGc8VTX-tj5bSAPnGKr2xb6--FMHiyjAQzsOQmtVSlAy1ajrw8AJLaMUcB3k02aXb7rco1OPXV2lByeQzhFknNo84-aQ8PAS46fbW0TYVfz-ilThtjC69mx5FDsV4bLwjiGq5AVUyjPagQaWhPeQh2uKs7C8KzDHUGe~ibxmhi7J3e1CKB9q7fwatqzyQYK12r3Fux9C~65kivQCXi2nc1x5eX9wBIIc3AzPKgVh9z2tdEQA__",
   },
   {
     id: 2,
     title: "December 10, 2020",
     subtitle: "The History Of Web Design",
-    image: "/public/card2.png",
+    image:
+      "https://s3-alpha-sig.figma.com/img/aca9/93d1/70e2ff047789addb5fdbe38039461050?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZcawRYor~gagWCZWHEf1IsXqm7122hDp4SbkVd63Xt4dLYNWEJM~dLU9hUmD4mfvQvekRIW2JFA0ZADPK1xajZ7BmB7O7nxNREzCiu9NagBxouI8LgEsAU66w~mRDhRwWjWVGkjH8yz0nWFGnq1MeaSV1MEqsi83FKZxzU2RkheumJuKBkTYgooXps1~LVQH8Exgbl8Azb07UzA-Aciy~XrB3PfNyaqYm7Z7kn1ClHyxyaMK3JkKQ1k3QOk6nGnzu3Nt6n5ePEiKEsRN1xMfyJWYJW8TNMMTvoZeYuCfLMg7CtQpHnCRmiX5S4XqBm~cAjOhEp1lDCyR9xHYU1x48w__",
   },
   {
     id: 3,
     title: "December 10, 2020",
     subtitle: "How to improve Web Design Process",
-    image: "/public/card3.png",
+    image:
+      "https://s3-alpha-sig.figma.com/img/e394/eec5/1407bf9ae31018c76bfe4b105a460162?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Zd63~KLo6NcmBfeb1k1Lh2lxxFoc4gGKkfDoeSI-jn7acTth2XvF~u8zkW~sIsJ1tBVL2ozQiDslzA0zTAB3Qil05QdkcpSssoIFzqS2Vt0wCd-GpWdEN3azZ5Q4pKuYbZ4lpTBNi0Vkn8RyzQ8e7~hB43OdUhdppv3gsmXjGYfPCDSpAGpk9uf7WUryFrre8X1W3aUv~86aLzci3QshxxEu4-TlqrS0Wf4ymM9qWZ8FON04M51V7aulPaeXCGMZVGS2nhXTH2xu1hvGC4gSzvyX1tMemUZrNKjyCFJnepffXrVq820TPbzDBWn8WqLMv7mV3PJg1pPNv~rjdaU4sw__",
   },
 ];
-
-
 
 export const practiceData = [
   {
     id: 1,
     title: "Intellectual Property",
-    subtitle: "Intellectual Property law deals with laws to protect creators and owners of inventions, writing, music, designs and other works.",
+    subtitle:
+      "Intellectual Property law deals with laws to protect creators and owners of inventions, writing, music, designs and other works.",
     image: "/public/IntelectualProperty.png",
   },
   {
     id: 2,
     title: "Real Estate",
-    subtitle: "Real estate law is a branch of civil law that covers the right to possess, use, and enjoy land.",
+    subtitle:
+      "Real estate law is a branch of civil law that covers the right to possess, use, and enjoy land.",
     image: "/public/RealEstate.png",
   },
   {
     id: 3,
     title: "Tax Law",
-    subtitle: "Tax law cover income, corporate, excise, luxury, estate and property taxes, to name a few.",
+    subtitle:
+      "Tax law cover income, corporate, excise, luxury, estate and property taxes, to name a few.",
     image: "/public/TaxLaw.png",
   },
 ];
